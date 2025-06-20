de67a2c898279b1240b9b0968be05fdcd3315184
Pedro Cavalcanti <pedropjc500@gmail.com>
2025-06-20 14:37:05 -0300

    improvement phone input

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,3 +1,21 @@
+const phone = document.getElementById("phone");
+
+phone.addEventListener("input", function(e) {
+    let input = e.target.value.replace(/\D/g, "");
+
+    if (input.length > 10) {
+        input = input.replace(/^(\d{2})(\d{5})(\d{4}).*/, "($1) $2-$3");
+    } else if (input.length > 6) {
+        input = input.replace(/^(\d{2})(\d{4})(\d{0,4}).*/, "($1) $2-$3");
+    } else if (input.length > 2) {
+        input = input.replace(/^(\d{2})(\d{0,5})/, "($1) $2");
+    } else {
+        input = input.replace(/^(\d*)/, "($1");
+    }
+
+    e.target.value = input;
+});
+
 const fileInput = document.getElementById('file');
 const fileNameDisplay = document.getElementById('file_name');
 
@@ -10,7 +28,9 @@ fileInput.addEventListener('change', () => {
     }
 });
 
-document.getElementById('register_form').addEventListener('submit', async function (event) {
+const registerForm = document.getElementById('register_form');
+
+registerForm.addEventListener('submit', async function (event) {
     event.preventDefault();
 
     const formData = new FormData(this);
